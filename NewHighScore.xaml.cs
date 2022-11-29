f34831f7a4dfbc85df739c47f7416aa5c36b9327
MaxPHed <114389278+MaxPHed@users.noreply.github.com>
2022-11-29 17:06:50 +0100

    k

--- NewHighScore.xaml.cs
+++ NewHighScore.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RalsShooterWindowMenu
 {
@@ -29,6 +30,8 @@ namespace RalsShooterWindowMenu
         int score;
         int bajsMackor;
         List<HighScore> highScoreList;
+        private bool BlinkOn = false;
+
 
         string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä", "Ö" };
 
@@ -41,12 +44,30 @@ namespace RalsShooterWindowMenu
             parent = game;
             HSStack.Children.Add(labelFixed);
             HSStack.Children.Add(labelPending);
+            labelPending.Content = alphabet[0];
             this.score = score;
             this.bajsMackor = bajsMackor;
 
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Start();
 
         }
-
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (BlinkOn)
+            {
+                labelPending.Foreground = Brushes.Black;
+                labelPending.Background = Brushes.White;
+            }
+            else
+            {
+                labelPending.Foreground = Brushes.White;
+                labelPending.Background = Brushes.Black;
+            }
+            BlinkOn = !BlinkOn;
+        }
         private void addLetter()
         {
             letter = alphabet[indexCounter];
